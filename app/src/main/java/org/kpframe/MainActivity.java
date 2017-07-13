5e0110ce95a049f3b412683aa6b6964a215ad92e
kaip121 <kaip121@126.com>
2017-07-13 16:05:38 +0800

    版本更新

--- app/src/main/java/org/kpframe/MainActivity.java
+++ app/src/main/java/org/kpframe/MainActivity.java
@@ -1,7 +1,17 @@
 package org.kpframe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kplike.library.common.prefser.FinestPrefser;
+import com.kplike.library.common.prefser.TypeToken;
+import com.kplike.library.common.storage.FinestStorage;
+import com.kplike.library.common.storage.Storage;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+
 }
